0a9af2d484c4d41c583398680586e56f49de2042
Nesat Nayem <nesatnayem042@gmail.com>
2022-05-14 19:21:40 +0600

    home

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './Pages/Header/Header';
-import Banner from './Pages/Banner/Banner';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Pages/Header/Header";
+
+import Footer from "./Pages/Footer/Footer";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div>
-    <BrowserRouter>
-    <Header />
-    <Banner />
-    <Routes>
-      <Route />
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+ 
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
