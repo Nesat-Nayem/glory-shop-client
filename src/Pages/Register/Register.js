20b2b4582448b569178b0d95426a854ae120e18b
Nesat Nayem <nesatnayem042@gmail.com>
2022-05-14 17:28:50 +0600

    register

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
-// import Swal from 'sweetalert2';
-
+import { useForm } from "react-hook-form";
+import Swal from 'sweetalert2';
 import useFirebase from '../../firebase/useFirebase';
-// import './Login.css'
+import './Register.css'
 
-const Login = () => {
+const Register = () => {
 
    const [userInfo, setUserInfo] = useState({});
-   const { SignInUser, signInWithGoogle } = useFirebase();
 
-   const handleonChange = (e) => {
+   const { createUser, signInWithGoogle } = useFirebase();
+
+   const handleBlur = (e) => {
       const field = e.target.name;
       const value = e.target.value;
       const newUser = { ...userInfo };
@@ -18,26 +19,46 @@ const Login = () => {
    };
 
    const handleSubmit = (e) => {
-      e.preventDefault();
-      SignInUser(userInfo.email, userInfo.password1);
-   }
+
+      if (userInfo.password1 !== userInfo.password2) {
+         Swal.fire("Opps!");
+         return;
+      } else {
+         createUser(userInfo.name, userInfo.email, userInfo.password1);
+         Swal.fire("User Created Successfully!");
+         e.preventDefault();
+      }
+   };
+
+
+
+
    return (
 
       <div className="container">
          <div className="row">
-            <div className="col-md-8 col-sm-12 col-lg-12 mx-auto">
+            <div className="col-md-12 col-sm-12 col-lg-12 mx-auto">
                <div className="myform form ">
                   <div className="logo mb-3">
                      <div className="col-md-12 text-center">
-                        <h1>Login</h1>
+                        <h1>Register</h1>
                      </div>
                   </div>
                   <form onSubmit={handleSubmit} name="login">
-
+                     <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Full Name</label>
+                        <input className='form-control'
+                           onBlur={handleBlur}
+                           type="text"
+                           name="name"
+                           placeholder="Full Name"
+                           required
+                        />
+                     </div>
                      <div className="form-group">
                         <label htmlFor="exampleInputEmail1">Email</label>
                         <input className='form-control'
-                           onBlur={handleonChange}
+                           onBlur={handleBlur}
                            type="email"
                            name="email"
                            placeholder="Email"
@@ -47,19 +68,28 @@ const Login = () => {
                      <div className="form-group">
                         <label htmlFor="exampleInputEmail1">Password</label>
                         <input className='form-control'
-                           onBlur={handleonChange}
+                           onBlur={handleBlur}
                            type="password"
                            name="password1"
                            placeholder="Password"
                            required
                         />
                      </div>
-
+                     <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Password</label>
+                        <input className='form-control'
+                           onBlur={handleBlur}
+                           type="password"
+                           name="password2"
+                           placeholder="Confirm Password"
+                           required
+                        />
+                     </div>
                      <div className="form-group">
                         <p className="text-center">By signing up you accept our <a href="##">Terms Of Use</a></p>
                      </div>
                      <div className="col-md-12 text-center ">
-                        <button type="submit" className=" btn px-4 btn-block mybtn btn-primary tx-tfm">Login</button>
+                        <button type="submit" className=" btn px-4 btn-block mybtn btn-primary tx-tfm">Register</button>
                      </div>
                      <div className="col-md-12 ">
                         <div className="login-or">
@@ -69,13 +99,12 @@ const Login = () => {
                      </div>
                      <div className="col-md-12 mb-3">
                         <p className="text-center">
-                           <span  onClick={signInWithGoogle} className="google btn mybtn"><i className="fab fa-google-plus">
+                           <a href='#' onClick={signInWithGoogle} className="google btn mybtn"><i className="fab fa-google-plus">
                            </i> Register using Google
-                           </span>
+                           </a>
                         </p>
                      </div>
                   </form>
-
 
                </div>
 
@@ -85,4 +114,4 @@ const Login = () => {
    );
 };
 
-export default Login;
+export default Register;
