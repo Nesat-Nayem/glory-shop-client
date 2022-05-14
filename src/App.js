a1f8a28200adbadaa0c4baed3f5f30e7a70e9c33
Nesat Nayem <nesatnayem042@gmail.com>
2022-05-14 18:14:32 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Pages/Header/Header';
+import Banner from './Pages/Banner/Banner';
 
 function App() {
   return (
     <div>
     <BrowserRouter>
     <Header />
+    <Banner />
     <Routes>
       <Route />
     </Routes>
